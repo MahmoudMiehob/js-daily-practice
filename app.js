94ecf3b0e2629d9c38f8931318e192234d429b2c
mahmoudmiehob <mahmoudmiehob@gmail.com>
2025-08-27 18:18:46 +0300

    Delete: line from app.js

--- app.js
+++ app.js
@@ -8,4 +8,5 @@ const colors = ['#f94144', '#f3722c', '#f9c74f', '#90be6d', '#577590'];
 btn.addEventListener('click', ()=>{
     const randomColor = colors[Math.floor(Math.random() * colors.length)];
     display.style.backgroundColor = randomColor;
+    
 });
